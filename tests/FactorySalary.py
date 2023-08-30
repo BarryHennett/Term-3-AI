8a2f1e1c93562142944d38e4a54b7bb9b2d5cac3
BarryHennett <124011454+BarryHennett@users.noreply.github.com>
2023-08-30 16:17:58 +1200

    updates, works

--- tests/FactorySalary.py
+++ tests/FactorySalary.py
@@ -5,52 +5,32 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
 from joblib import dump, load
-
-file_path = "C:/Users/harra/Desktop/Term-3-AI/Car_Purchasing_Data.xlsx"
-
+import numpy as np
 
 class ModelFactory:
-    def create_model(self, model_type):
-        if model_type == 'LinearRegression':
+    @staticmethod
+    def get_model(model_name):
+        if model_name == 'Linear Regression':
             return LinearRegression()
-        elif model_type == 'SVR':
+        elif model_name == 'Support Vector Machine':
             return SVR()
-        elif model_type == 'RandomForest':
+        elif model_name == 'Random Forest':
             return RandomForestRegressor()
-        elif model_type == 'GradientBoosting':
+        elif model_name == 'Gradient Boosting Regressor':
             return GradientBoostingRegressor()
-        elif model_type == 'XGB':
+        elif model_name == 'XGBRegressor':
             return XGBRegressor()
         else:
-            raise ValueError(f"Invalid model type: {model_type}")
+            raise ValueError(f"Model '{model_name}' not recognized!")
 
-# Method to train models
-def train_models(X_train, y_train):
-    lr = LinearRegression()
-    svm = SVR()
-    rf = RandomForestRegressor()
-    gbr = GradientBoostingRegressor()
-    xg = XGBRegressor()
-    
-    lr.fit(X_train, y_train)
-    svm.fit(X_train, y_train)
-    rf.fit(X_train, y_train)
-    gbr.fit(X_train, y_train)
-    xg.fit(X_train, y_train)
-    
-    return lr, svm, rf, gbr, xg
+def load_data(file_name):
+    return pd.read_excel(file_name)
 
-
-# Method to load data
-def load_data(file_path):
-    data = pd.read_excel(file_path)
-    return data
-
-# Method to preprocess data
 def preprocess_data(data):
     X = data.drop(['Customer Name', 'Customer e-mail', 'Country', 'Car Purchase Amount'], axis=1)
     Y = data['Car Purchase Amount']
@@ -64,32 +44,45 @@ def preprocess_data(data):
     
     return X_scaled, y_scaled, sc, sc1
 
-# Method to split data into training and testing sets
-def split_data(X_scaled, y_scaled, test_size=0.2, random_state=42):
-    X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_scaled, test_size=test_size, random_state=random_state)
-    return X_train, X_test, y_train, y_test
+def split_data(X_scaled, y_scaled):
+    return train_test_split(X_scaled, y_scaled, test_size=0.2, random_state=42)
 
-# Method to evaluate model performance
+def train_models(X_train, y_train):
+    model_names = [
+        'Linear Regression',
+        'Support Vector Machine',
+        'Random Forest',
+        'Gradient Boosting Regressor',
+        'XGBRegressor'
+    ]
+    
+    models = {}
+    for name in model_names:
+        model = ModelFactory.get_model(name)
+        model.fit(X_train, y_train.ravel())
+        models[name] = model
+        
+    return models
+
 def evaluate_models(models, X_test, y_test):
-    model_names = ['Linear Regression', 'Support Vector Machine', 'Random Forest', 'Gradient Boosting Regressor', 'XGBRegressor']
-    rmse_values = []
+    rmse_values = {}
     
-    for model in models:
+    for name, model in models.items():
         preds = model.predict(X_test)
-        rmse = mean_squared_error(y_test, preds, squared=False)
-        rmse_values.append(rmse)
-    
-    return model_names, rmse_values
+        rmse_values[name] = mean_squared_error(y_test, preds, squared=False)
+        
+    return rmse_values
 
-# Method to plot model performance
-def plot_model_performance(model_names, rmse_values):
-    plt.figure(figsize=(10, 7))
-    bars = plt.bar(model_names, rmse_values, color=['blue', 'green', 'red', 'purple', 'orange'])
-    
+def plot_model_performance(rmse_values):
+    plt.figure(figsize=(10,7))
+    models = list(rmse_values.keys())
+    rmse = list(rmse_values.values())
+    bars = plt.bar(models, rmse, color=['blue', 'green', 'red', 'purple', 'orange'])
+
     for bar in bars:
         yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width() / 2, yval + 0.00001, round(yval, 5), ha='center', va='bottom', fontsize=10)
-    
+        plt.text(bar.get_x() + bar.get_width()/2, yval + 0.00001, round(yval, 5), ha='center', va='bottom', fontsize=10)
+
     plt.xlabel('Models')
     plt.ylabel('Root Mean Squared Error (RMSE)')
     plt.title('Model RMSE Comparison')
@@ -97,53 +90,29 @@ def plot_model_performance(model_names, rmse_values):
     plt.tight_layout()
     plt.show()
 
-# Method to save the best model
 def save_best_model(models, rmse_values):
-    best_model_index = rmse_values.index(min(rmse_values))
-    best_model_object = models[best_model_index]
+    best_model_name = min(rmse_values, key=rmse_values.get)
+    best_model = models[best_model_name]
+    dump(best_model, "car_model.joblib")
+
+def predict_new_data(loaded_model, sc, sc1):
+    X_test1 = sc.transform(np.array([[0,42,62812.09301,11609.38091,238961.2505]]))
+    pred_value = loaded_model.predict(X_test1)
+    print(pred_value)
     
-    dump(best_model_object, "car_model.joblib")
+    # Ensure pred_value is a 2D array before inverse transform
+    if len(pred_value.shape) == 1:
+        pred_value = pred_value.reshape(-1, 1)
 
-# Method to predict new data
-def predict_new_data(model, scaler_x, scaler_y, input_data):
-    input_data_scaled = scaler_x.transform(input_data)
-    pred_value = model.predict(input_data_scaled)
-    pred_value_rescaled = scaler_y.inverse_transform(pred_value)
-    return pred_value_rescaled
+    print("Predicted output: ", sc1.inverse_transform(pred_value))
 
-# Load the data
-data = load_data('Car_Purchasing_Data.xlsx')
-
-# Preprocess the data
-X_scaled, y_scaled, sc, sc1 = preprocess_data(data)
-
-# Split data into training and testing sets
-X_train, X_test, y_train, y_test = split_data(X_scaled, y_scaled)
-
-# Train models
-model_factory = ModelFactory()
-models = train_models(X_train, y_train)
-
-# Evaluate model performance
-model_names, rmse_values = evaluate_models(models, X_test, y_test)
-
-# Plot model performance
-plot_model_performance(model_names, rmse_values)
-
-# Save the best model
-save_best_model(models, rmse_values)
-
-# Load the best model
-loaded_model = load("car_model.joblib")
-
-# Gather user inputs
-gender = int(input("Enter gender (0 for female, 1 for male): "))
-age = int(input("Enter age: "))
-annual_salary = float(input("Enter annual salary: "))
-credit_card_debt = float(input("Enter credit card debt: "))
-net_worth = float(input("Enter net worth: "))
-
-# Predict on new data
-input_data = [[gender, age, annual_salary, credit_card_debt, net_worth]]
-pred_value = predict_new_data(loaded_model, sc, sc1, input_data)
-print("Predicted Car Purchase Amount based on input:", pred_value)
+if __name__ == "__main__":
+    data = load_data('C:/Users/harra/Desktop/Term-3-AI/Car_Purchasing_Data.xlsx')
+    X_scaled, y_scaled, sc, sc1 = preprocess_data(data)
+    X_train, X_test, y_train, y_test = split_data(X_scaled, y_scaled)
+    models = train_models(X_train, y_train)
+    rmse_values = evaluate_models(models, X_test, y_test)
+    plot_model_performance(rmse_values)
+    save_best_model(models, rmse_values)
+    loaded_model = load("car_model.joblib")
+    predict_new_data(loaded_model, sc, sc1)
