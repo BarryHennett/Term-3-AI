35bcc5b5ccdd9dea4d28b11b0d108cadfde0587b
BarryHennett <124011454+BarryHennett@users.noreply.github.com>
2023-09-05 09:56:22 +1200

    var change

--- tests/test_Factory.py
+++ tests/test_Factory.py
@@ -7,19 +7,19 @@ def test_load_data():
     data = load_data('Car_Purchasing_Data.xlsx')
     assert isinstance(data, pd.DataFrame), "Loaded data should be a DataFrame."
 
-    expected_columns = ['Customer Name', 'Customer e-mail', 'Country', 'Gender', 
+    Col_Exp = ['Customer Name', 'Customer e-mail', 'Country', 'Gender', 
                         'Annual Salary', 'Credit Card Debt', 'Net Worth', 'Car Purchase Amount']
 
-    for col in expected_columns:
+    for col in Col_Exp:
         assert col in data.columns, f"Expected column {col} not found in loaded data."
 
 #Test case to ensure the correct shape of data
 def test_shape_of_data():
     data = load_data('Car_Purchasing_Data.xlsx')
-    X_scaled, y_scaled, _, _ = preprocess_data(data)
+    sca_X, sca_Y, _, _ = preprocess_data(data)
     
-    assert X_scaled.shape[1] == 5, "Expected 5 features in the X data after preprocessing."
-    assert y_scaled.shape[1] == 1, "Expected Y data to have a single column."
+    assert sca_X.shape[1] == 5, "Expected 5 features in the X data after preprocessing."
+    assert sca_Y.shape[1] == 1, "Expected Y data to have a single column."
 
 #Test case to ensure the correct columns for Input
 def test_columns_X():
@@ -27,43 +27,43 @@ def test_columns_X():
     X, _, _, _ = preprocess_data(data)
     input_columns = ['Gender', 'Age', 'Annual Salary', 'Credit Card Debt', 'Net Worth']
     # Convert the NumPy array to a DataFrame
-    X_df = pd.DataFrame(X, columns=input_columns)
-    # Check if X_df is a DataFrame
-    assert isinstance(X_df, pd.DataFrame)
+    X_DataFrame = pd.DataFrame(X, columns=input_columns)
+    # Check if X_DataFrame is a DataFrame
+    assert isinstance(X_DataFrame, pd.DataFrame)
     # Check that the columns have been dropped for X
-    assert "Customer Name" not in X_df.columns
-    assert "Customer e-mail" not in X_df.columns
-    assert "Country" not in X_df.columns
-    assert "Car Purchase Amount" not in X_df.columns
+    assert "Customer Name" not in X_DataFrame.columns
+    assert "Customer e-mail" not in X_DataFrame.columns
+    assert "Country" not in X_DataFrame.columns
+    assert "Car Purchase Amount" not in X_DataFrame.columns
 
 #Test case to ensure the correct column for output
 def test_columns_Y():
     data = load_data('Car_Purchasing_Data.xlsx')
     _, Y, _, _ = preprocess_data(data)
     # Convert the NumPy array to a DataFrame
-    Y_df = pd.DataFrame(Y, columns=['Car Purchase Amount'])
+    Y_DataFrame = pd.DataFrame(Y, columns=['Car Purchase Amount'])
     
-    # Check if Y_df is a DataFrame and has the correct column name
-    assert isinstance(Y_df, pd.DataFrame)
-    assert Y_df.columns == 'Car Purchase Amount'
+    # Check if Y_DataFrame is a DataFrame and has the correct column name
+    assert isinstance(Y_DataFrame, pd.DataFrame)
+    assert Y_DataFrame.columns == 'Car Purchase Amount'
 
 #Test case to ensure the correct range of data
 def test_data_range():
     data = load_data('Car_Purchasing_Data.xlsx')
-    X_scaled, y_scaled, _, _ = preprocess_data(data)
+    sca_X, sca_Y, _, _ = preprocess_data(data)
     
-    assert 0 <= np.min(X_scaled) <= 1, "X data should be scaled between 0 and 1."
-    assert 0 <= np.min(y_scaled) <= 1, "Y data should be scaled between 0 and 1."
-    assert 0 <= np.max(X_scaled) <= 1, "X data should be scaled between 0 and 1."
-    assert 0 <= np.max(y_scaled) <= 1, "Y data should be scaled between 0 and 1."
+    assert 0 <= np.min(sca_X) <= 1, "X data should be scaled between 0 and 1."
+    assert 0 <= np.min(sca_Y) <= 1, "Y data should be scaled between 0 and 1."
+    assert 0 <= np.max(sca_X) <= 1, "X data should be scaled between 0 and 1."
+    assert 0 <= np.max(sca_Y) <= 1, "Y data should be scaled between 0 and 1."
 
 #Test case to ensure the correct splitting of data
 def test_data_split():
     data = load_data('Car_Purchasing_Data.xlsx')
     X, Y, _, _ = preprocess_data(data)
-    X_train, X_test, y_train, y_test = split_data(X, Y)
+    Train_X, Test_X, Train_Y, Test_Y = split_data(X, Y)
     # Check proportions for train-test split
-    assert X_train.shape[0] / X.shape[0] == 0.8
-    assert X_test.shape[0] / X.shape[0] == 0.2
-    assert y_train.shape[0] / Y.shape[0] == 0.8
-    assert y_test.shape[0] / Y.shape[0] == 0.2
+    assert Train_X.shape[0] / X.shape[0] == 0.8
+    assert Test_X.shape[0] / X.shape[0] == 0.2
+    assert Train_Y.shape[0] / Y.shape[0] == 0.8
+    assert Test_Y.shape[0] / Y.shape[0] == 0.2
